e3badd3826b102d870b04b206146418886a820ff
Jack Willis <66927986+jackwillis517@users.noreply.github.com>
2022-02-18 16:11:43 -0500

    update

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+
+
+function App() {
+    return ( 
+    <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" exact element={ <Login />  } />
+                <Route path="/register" exact element={ <Register />  } />
+            </Routes>
+        </BrowserRouter>
+    </div>
+    )
+}
+
+export default App;
